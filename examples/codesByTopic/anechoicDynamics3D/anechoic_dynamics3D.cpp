64bee751c42c53d9aa0e6e5e5e7802cdd7a72a53
José Pedro <1jpsneto@gmail.com>
2016-08-06 22:16:18 -0300

    Anechoic test with sinusoidal signal done

--- examples/codesByTopic/anechoicDynamics3D/anechoic_dynamics3D.cpp
+++ examples/codesByTopic/anechoicDynamics3D/anechoic_dynamics3D.cpp
@@ -17,6 +17,9 @@ typedef Array<T,3> Velocity;
 using namespace plb_acoustics_3D;
 // ---------------------------------------------
 
+const T rho0 = 1;
+const T drho = rho0*7;
+
 void writeGifs(MultiBlockLattice3D<T,DESCRIPTOR>& lattice, plint iter){
     const plint nx = lattice.getNx();
     const plint ny = lattice.getNy();
@@ -25,10 +28,13 @@ void writeGifs(MultiBlockLattice3D<T,DESCRIPTOR>& lattice, plint iter){
     const plint imSize = 600;
     ImageWriter<T> imageWriter("leeloo");
     
-    // Write velocity-norm at x=0.
-    Box3D slice(0, nx-1, 0, ny-1, 50, 50);
-    imageWriter.writeScaledGif( createFileName("rho", iter, 6),
-    *computeDensity(lattice, slice), imSize, imSize);
+    Box3D slice(0, nx-1, 0, ny-1, nz/2, nz/2);
+    //imageWriter.writeGif(createFileName("u", iT, 6),
+     //*computeDensity(lattice), );
+
+    imageWriter.writeGif( createFileName("rho", iter, 6),
+    *computeDensity(lattice, slice), 
+    (T) rho0 - drho/1000000, (T) rho0 + drho/1000000, imSize, imSize);
 }
 
 void writeVTK(MultiBlockLattice3D<T,DESCRIPTOR>& lattice, plint iter){
@@ -47,10 +53,7 @@ int main(int argc, char **argv){
     const T lattice_speed_sound = 1/sqrt(3);
 
     const T omega = 1.0;
-    const T rho0 = 1;
-    const T drho = rho0/10;
-
-    const plint maxT = 20000;
+    const plint maxT = 210*2;
 
     const T lx =  2.3;
     const T dx =lx/nx;
@@ -65,7 +68,8 @@ int main(int argc, char **argv){
     global::directories().setOutputDir(fNameOut+"/");
 
     pcout << "Creation of the lattice." << endl;
-    MultiBlockLattice3D<T,DESCRIPTOR> lattice(nx,ny,nz, new CompleteBGKdynamics<T,DESCRIPTOR>(omega));
+    MultiBlockLattice3D<T,DESCRIPTOR> lattice(nx,ny,nz, 
+        new CompleteBGKdynamics<T,DESCRIPTOR>(omega));
 
     // Switch off periodicity.
     lattice.periodicity().toggleAll(false);
@@ -90,18 +94,61 @@ int main(int argc, char **argv){
 
     pcout << "Simulation begins" << endl;
     for (plint iT=0; iT<maxT; ++iT){
-            
-        if (iT== 0){
-            Box3D impulse( 50, 50 , 50, 50 , 50,50);
-            initializeAtEquilibrium( lattice, impulse, rho0+drho*5 , u0 );
-            lattice.initialize();
+        T lattice_speed_sound = 1/sqrt(3);
+        T rho_changing = rho0 + drho*sin(2*M_PI*4*(lattice_speed_sound/20)*iT);        
+        Box3D impulse(nx/2, nx/2, ny/2, ny/2, nz/2, nz/2);
+        initializeAtEquilibrium( lattice, impulse, rho_changing, u0 );
 
-        }
 
         if (iT % 5 == 0 && iT>0) {
             pcout << "Iteration " << iT << endl;
             writeGifs(lattice,iT);
-            writeVTK(lattice, iT);
+            //writeVTK(lattice, iT);
+        }
+
+        if (iT == 200){
+            plb_ofstream sinusoidal_001_points("tmp/sinusoidal_001_points.dat");
+            plb_ofstream sinusoidal_010_points("tmp/sinusoidal_010_points.dat");
+            plb_ofstream sinusoidal_011_points("tmp/sinusoidal_011_points.dat");
+            plb_ofstream sinusoidal_100_points("tmp/sinusoidal_100_points.dat");
+            plb_ofstream sinusoidal_101_points("tmp/sinusoidal_101_points.dat");
+            plb_ofstream sinusoidal_110_points("tmp/sinusoidal_110_points.dat");
+            plb_ofstream sinusoidal_111_points("tmp/sinusoidal_111_points.dat");
+
+            for (plint z = 0; z < nz; ++z){
+                for (plint y = 0; y < ny; ++y){
+                    for (plint x = 0; x < nx; ++x){
+                        if (x == 0 && y == 0){
+                            sinusoidal_001_points << setprecision(10) <<
+                            lattice.get(x,y,z).computeDensity() - rho0 << endl;
+                        }
+                        if (x == 0 && z == 0){
+                            sinusoidal_010_points << setprecision(10) <<
+                            lattice.get(x,y,z).computeDensity() - rho0 << endl;
+                        }
+                        if (x == 0 && y == z){
+                            sinusoidal_011_points << setprecision(10) <<
+                            lattice.get(x,y,z).computeDensity() - rho0 << endl;
+                        }
+                        if (y == 0 && z == 0){
+                            sinusoidal_100_points << setprecision(10) <<
+                            lattice.get(x,y,z).computeDensity() - rho0 << endl;
+                        }
+                        if (x == z && y == 0){
+                            sinusoidal_101_points << setprecision(10) <<
+                            lattice.get(x,y,z).computeDensity() - rho0 << endl;
+                        }
+                        if (x == y && z == 0){
+                            sinusoidal_110_points << setprecision(10) <<
+                            lattice.get(x,y,z).computeDensity() - rho0 << endl;
+                        }
+                        if (x == y && z == x){
+                            sinusoidal_111_points << setprecision(10) <<
+                            lattice.get(x,y,z).computeDensity() - rho0 << endl;
+                        }
+                    }
+                }                
+            }
         }
 
         lattice.collideAndStream();
