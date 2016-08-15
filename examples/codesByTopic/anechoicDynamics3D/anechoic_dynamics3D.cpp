7ac141a91bc57b9f39017824185f05bd0606ca7b
José Pedro <1jpsneto@gmail.com>
2016-08-15 09:17:00 -0300

    Investigating bug for bounce back in D3QX

--- examples/codesByTopic/anechoicDynamics3D/anechoic_dynamics3D.cpp
+++ examples/codesByTopic/anechoicDynamics3D/anechoic_dynamics3D.cpp
@@ -29,12 +29,12 @@ void writeGifs(MultiBlockLattice3D<T,DESCRIPTOR>& lattice, plint iter){
     ImageWriter<T> imageWriter("leeloo");
     
     Box3D slice(0, nx-1, 0, ny-1, nz/2, nz/2);
-    //imageWriter.writeGif(createFileName("u", iT, 6),
-     //*computeDensity(lattice), );
+    imageWriter.writeScaledGif( createFileName("velocity", iter, 6),
+    *computeDensity(lattice, slice), imSize, imSize);
 
     imageWriter.writeGif( createFileName("rho", iter, 6),
     *computeDensity(lattice, slice), 
-    (T) rho0 - drho/1000000, (T) rho0 + drho/1000000, imSize, imSize);
+    (T) rho0 - drho/100000, (T) rho0 + drho/100000, imSize, imSize);
 }
 
 void writeVTK(MultiBlockLattice3D<T,DESCRIPTOR>& lattice, plint iter){
@@ -52,8 +52,8 @@ int main(int argc, char **argv){
     const plint nz = 100;
     const T lattice_speed_sound = 1/sqrt(3);
 
-    const T omega = 1.0;
-    const plint maxT = 210*2;
+    const T omega = 1.9;
+    const plint maxT = 120000;
 
     const T lx =  2.3;
     const T dx =lx/nx;
@@ -77,14 +77,23 @@ int main(int argc, char **argv){
     pcout << "Initilization of rho and u." << endl;
     initializeAtEquilibrium( lattice, lattice.getBoundingBox(), rho0 , u0 );
 
+    plint size_square = 4;
+    Box3D square(
+    nx/2 - size_square/2, nx/2 + size_square/2,
+    ny/2 - size_square/2, ny/2 + size_square/2, 
+    nz/2 - size_square/2, nz/2 + size_square/2);
+    defineDynamics(lattice, square, new BounceBack<T,DESCRIPTOR>((T)0));
+
     // Anechoic Condition
     T rhoBar_target = 0;
-    Array<T,3> j_target(0, 0, 0);
+    const T mach_number = 0.2;
+    const T velocity_flow = mach_number*lattice_speed_sound;
+    Array<T,3> j_target(velocity_flow, 0.0/std::sqrt(3), 0.0/std::sqrt(3));
     T size_anechoic_buffer = 30;
     // Define Anechoic Boards
-    /*defineAnechoicBoards(nx, ny, nz, lattice, size_anechoic_buffer,
+    defineAnechoicBoards(nx, ny, nz, lattice, size_anechoic_buffer,
       omega, j_target, j_target, j_target, j_target, j_target, j_target,
-      rhoBar_target);*/
+      rhoBar_target);
     
 
     lattice.initialize();
@@ -97,15 +106,23 @@ int main(int argc, char **argv){
     plb_ofstream history_pressures("history_pressures.dat");
     for (plint iT=0; iT<maxT; ++iT){
         if (iT == 0){
-            Box3D impulse(nx/2, nx/2, ny/2, ny/2, nz/2, nz/2);
-            initializeAtEquilibrium( lattice, impulse, rho0 + drho, u0 );
+            //Box3D impulse(nx/2, nx/2, ny/2, ny/2, nz/2, nz/2);
+            //initializeAtEquilibrium( lattice, impulse, rho0 + drho, u0 );
         }
 
-        if (iT % 5 == 0 && iT>0) {
+        if (iT % 100 == 0 && iT>0) {
             pcout << "Iteration " << iT << endl;
+              pcout << " energy ="
+                  << setprecision(10) << getStoredAverageEnergy<T>(lattice)
+                  << " rho ="
+                  << getStoredAverageDensity<T>(lattice)
+                  << " max_velocity ="
+                  << setprecision(10) << (getStoredMaxVelocity<T>(lattice))/lattice_speed_sound
+                  << endl;
             writeGifs(lattice,iT);
-            writeVTK(lattice, iT);
+            //writeVTK(lattice, iT);
         }
+
 
         history_pressures << setprecision(10) << lattice.get(nx/2+30, ny/2+30, nz/2+30).computeDensity() - rho0 << endl;
         lattice.collideAndStream();
