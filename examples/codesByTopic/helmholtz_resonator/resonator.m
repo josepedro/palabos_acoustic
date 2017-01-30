5d8204f018d07a989e5f7a60cb561cd518e40036
José Pedro <1jpsneto@gmail.com>
2017-01-30 15:44:26 -0200

    Adding ressonator

--- examples/codesByTopic/helmholtz_resonator/resonator.m
+++ examples/codesByTopic/helmholtz_resonator/resonator.m
@@ -1,23 +1,40 @@
 % find frequency resonator
 close all
 clear('all');
-pressures = textread('velocities_y.dat');
-pressures = pressures - mean(pressures);
-window = hanning(length(pressures));
-pressures = pressures.*window;
-velocities = textread('velocities_y.dat');
-velocities = velocities - mean(velocities);
-velocities = velocities.*window;
-%impedance = fft(pressures)./fft(velocities);
+pressures_in = textread('pressures_in.dat');
+pressures_in = pressures_in - rms(pressures_in);
+pressures_out = textread('pressures_out.dat');
+pressures_out = pressures_out - rms(pressures_out);
+
+A = 1;
+B = 20;
+windowLength = length(pressures_in)/A;
+Sxx_in = cpsd(pressures_in, pressures_in, hann(windowLength), windowLength/B, length(pressures_in), 1, 'twosided');
+psdx_in = abs(Sxx_in(1:length(pressures_in)/2+1));
+fft_pressures_in = sqrt(psdx_in*1*length(pressures_in));
+
+windowLength = length(pressures_out)/A;
+Sxx_out = cpsd(pressures_out, pressures_out, hann(windowLength), windowLength/B, length(pressures_out), 1, 'twosided');
+psdx_out = abs(Sxx_out(1:length(pressures_out)/2+1));
+fft_pressures_out = sqrt(psdx_out*1*length(pressures_out));
 
 
-N=length(pressures);
-windowLenght=N/80;
-%Sxx = cpsd(pressures,pressures,hanning(windowLenght),N/90,N,'twosided');
-%Sxx = cpsd(pressures,pressures);
-frequencies = linspace(0, 1, length(pressures));
+window = hanning(length(pressures_in));
+pressures_in = pressures_in.*window;
+pressures_out = pressures_out.*window;
+
+transmission_coefficient_2 = (abs(fft_pressures_out./fft_pressures_in)).^2;
+transmission_loss_2 = 10*log10(1./transmission_coefficient_2);
+
+transmission_coefficient_1 = (abs(fft(pressures_out)./fft(pressures_in))).^2;
+transmission_loss_1 = 10*log10(1./transmission_coefficient_1);
+
+frequencies_2 = linspace(0, 1, length(transmission_coefficient_2));
+frequencies_1 = linspace(0, 1, length(transmission_coefficient_1));
 figure(1);
-%plot(frequencies,abs(Sxx));
-plot(frequencies, abs(fft(pressures)));
-xlim([0 0.1]);
+%plot(frequencies,abs(Sxx));smooth(real(Zr), 7)
+plot(frequencies_1, transmission_loss_1, 'r');
+hold on;
+plot(frequencies_1, smooth(transmission_loss_1, 30));
+xlim([0 0.5]);
 %ylim([0 0.08])
