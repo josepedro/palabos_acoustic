38ec07c6aac064e2332fd84c6d7f83a6cae7298f
José Pedro <1jpsneto@gmail.com>
2017-05-18 18:14:09 -0300

    Updating

--- examples/codesByTopic/duct_radiation_optimization/duct_radiation_optimization.cpp
+++ examples/codesByTopic/duct_radiation_optimization/duct_radiation_optimization.cpp
@@ -41,12 +41,7 @@ int main(int argc, char **argv){
     const T Re = atof(argv[3]);
     pcout << Re << endl;
     const T tau = (0.5 + ((velocity_flow*diameter)/(Re*lattice_speed_sound*lattice_speed_sound)));
-    T omega;
-    if (Re == 0 && mach_number == 0) {
-      omega = 1.99;
-    }else{
-      omega = 1/tau;
-    }
+    T omega = atof(argv[3]);
 
     const T rho0 = 1;
     const T drho = compute_drho(80); // NPS dB
@@ -62,8 +57,8 @@ int main(int argc, char **argv){
     const plint length_duct = 3*(3*diameter);
     //const plint length_duct = 3*diameter;
 
-    const plint nz = length_duct + (60/2)*diameter + 30; // precious
-    //const plint nz = length_duct + 3*diameter + 30;
+    //const plint nz = length_duct + (60/2)*diameter + 30; // precious
+    const plint nz = length_duct + 3*diameter + 30;
     //const T omega = 1.985;
 
     const plint maxT = 2*(pow(2,13) + nz*sqrt(3));
